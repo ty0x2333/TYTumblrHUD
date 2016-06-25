7fab6783f91e36b1da41be9477b27c0102f97c84
luckytianyiyan <luckytianyiyan@gmail.com>
2016-06-25 17:57:24 +0800

    Initial Commit

---
+++ TYTumblrHUD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TYTumblrHUD
+//
+//  Created by luckytianyiyan on 16/6/25.
+//  Copyright © 2016年 luckytianyiyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TYTumblrHUD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TYTumblrHUD
+//
+//  Created by luckytianyiyan on 16/6/25.
+//  Copyright © 2016年 luckytianyiyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
