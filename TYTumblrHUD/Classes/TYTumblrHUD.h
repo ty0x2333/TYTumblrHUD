40b0995a688821a111c7e804b00049bad67f9927
luckytianyiyan <luckytianyiyan@gmail.com>
2018-02-10 18:06:21 +0800

    Add color property

--- TYTumblrHUD/Classes/TYTumblrHUD.h
+++ TYTumblrHUD/Classes/TYTumblrHUD.h
@@ -8,6 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface TYTumblrHUD : UIView
 
 @property (nonatomic, assign) BOOL removeFromSuperViewOnHide;
@@ -22,4 +24,8 @@
 
 - (void)hideAnimated:(BOOL)animated;
 
+@property (nonatomic, strong) UIColor *color;
+
 @end
+
+NS_ASSUME_NONNULL_END
